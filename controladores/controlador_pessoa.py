b72cd980d90eb1710af2b77f3cebeb6337db264c
guicschlickmann <130292818+guicschlickmann@users.noreply.github.com>
2023-05-11 11:14:41 -0300

    criei abre_tela e estou criando os outros metodos

--- controladores/controlador_pessoa.py
+++ controladores/controlador_pessoa.py
@@ -8,6 +8,15 @@ class ControladorPessoa():
         self.__doadores = list()
         self.__tela_pessoa = TelaPessoa()
         self.__controlador_sistema = controlador_sistema
-    
+
     def incluir_adotante(self):
         dados_pessoa = self.__tela_pessoa.pega_dados_adotante()
+
+    def abre_tela(self):
+        lista_opcoes = {1: self.incluir_adotante, 2: self.altera_adotante, 3: self.lista_adotante, 4: self.excluir_adotante,
+                        5: self.incluir_doador, 6: self.altera_doador, 7: self.lista_doador, 8: self.excluir_doador,
+                        0: self.retornar}
+
+        continua = True
+        while continua:
+            lista_opcoes[self.__tela_livro.tela_opcoes()]()
